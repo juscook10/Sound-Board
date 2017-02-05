e07a40352113cdab33af1e179785395b9bb7665b
Justin Cook <juscook10@gmail.com>
2017-02-04 22:33:37 -0600

    Setup Complete
    Added the interface.

---
+++ Sound Board/SoundViewController.swift
@@ -0,0 +1,33 @@
+//
+//  SoundViewController.swift
+//  Sound Board
+//
+//  Created by Justin Cook on 2/4/17.
+//  Copyright © 2017 Justin Allen Designs. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var recordButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+    
+    @IBAction func playTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+
+}
